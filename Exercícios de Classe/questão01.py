8c83c314ec3a0fc6fd4511b6f82d06b167699584
Leo Marinho <dev.leomarinho@gmail.com>
2023-10-16 21:14:34 -0300

    Correção do exercício de sala

--- Exercícios de Classe/questão01.py
+++ Exercícios de Classe/questão01.py
@@ -4,6 +4,16 @@ q = []
 for i in range(20):
     q.append(randint(1,100))
 
+maior = -1
+menor = 101
+
+for i in q:
+    if maior < i:
+        maior = i
+
+    if menor > i:
+        menor = i
+
 print('Vetor Q =', q)
-print('O valor do maior número no vetor Q é =', max(q), 'e sua posição na lista é =', q.index(max(q)))
-print('O valor do menor número no vetor Q é =', min(q), 'e sua posição na lista é =', q.index(min(q)))
+print(f'O maior número da lista é {maior} e sua posição na lista é {q.index(maior)}')
+print(f'O menor número da lista é {menor} e sua posição na lista é {q.index(menor)}')
